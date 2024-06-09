601650ac4eb74d563150f367f68168624eaa2b9b
Marina Che <acc13072021@gmail.com>
2024-06-09 15:26:48 +0300

    6.Add localization in at least two languages for email templates and the index.html start page.

--- src/main/java/com/javarush/jira/common/internal/config/LocaleConfiguration.java
+++ src/main/java/com/javarush/jira/common/internal/config/LocaleConfiguration.java
@@ -1,6 +1,5 @@
 package com.javarush.jira.common.internal.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
