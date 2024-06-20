44136ac9be568d2c32fc4b1e8420ea0e7fe5077c
Marina Che <acc13072021@gmail.com>
2024-06-20 16:06:57 +0300

    10.Add time counting: how long the task was in work and testing.

--- README.md
+++ README.md
@@ -28,28 +28,22 @@
 
 Список выполненных задач:
 1.Remove "vk","yandex".
-Знімок екрана 2024-06-06 203523
 
 2.Extract sensitive information to a separate property file.
-Знімок екрана 2024-06-06 203523
 
 3.Write tests for all public methods of the ProfileRestController.
-Знімок екрана 2024-06-07 144623
 
 4.Refactor the method FileUtil#upload to a modern approach to working with the file system.
-Знімок екрана 2024-06-07 144623
 
 5.Add new functionality: adding tags to the task: REST API + implementation on the service.
-Знімок екрана 2024-06-07 144623
 
 6.Add localization in at least two languages for email templates and the index.html start page.
-Знімок екрана 2024-06-09 124215
 
 7.Write a Dockerfile for the primary server.
-Знімок екрана 2024-06-12 184440
 
 8.Write a docker-compose file to run the server container together with the database and nginx. For nginx, use the config file config/nginx.conf.
-Знімок екрана 2024-06-12 184440
 
-9.Rework the tests so that the in-memory database is used during the tests (H2)
+9.Rework the tests so that the in-memory database is used during the tests (H2).
+
+10.Add time counting: how long the task was in work and testing.
 ...
--- src/main/resources/data4dev/data.sql
+++ src/main/resources/data4dev/data.sql
@@ -30,8 +30,10 @@ insert into USER_ROLE (USER_ID, ROLE)
 values (1, 0),
        (2, 0),
        (2, 1),
+       (4, 2),
        (5, 0),
        (6, 0),
+       (7, 0),
        (8, 0),
        (9, 0),
        (10, 0),
@@ -41,6 +43,7 @@ values (1, 0),
        (14, 0),
        (15, 1),
        (16, 1);
+
 
 insert into PROFILE (ID, LAST_FAILED_LOGIN, LAST_LOGIN, MAIL_NOTIFICATIONS)
 values (1, null, null, 49),
@@ -61,9 +64,14 @@ insert into ATTACHMENT (name, file_link, object_id, object_type, user_id, date_t
 values ('Снимок экрана 1.png', './attachments/project/1_Снимок экрана 1.png', 2, 0, 4, '2023-05-04 22:28:50.215429'),
        ('Снимок экрана 2.png', './attachments/project/2_Снимок экрана 2.png', 2, 0, 4, '2023-05-04 22:28:53.687600'),
        ('Ежедневный-чеклист.xlsx', './attachments/project/3_Ежедневный-чеклист.xlsx', 2, 0, 4,
-        '2023-05-04 22:31:15.166547');
+        '2023-05-04 22:31:15.166547'),
+       ('Снимок экрана 1.png', './attachments/task/1_Снимок экрана 1.png', 41, 2, 4, '2023-05-04 22:28:53.687600'),
+       ('Снимок экрана 2.png', './attachments/task/2_Снимок экрана 2.png', 41, 2, 4, '2023-05-04 22:28:50.215429'),
+       ('Ежедневный-чеклист.xlsx', './attachments/task/3_Ежедневный-чеклист.xlsx', 38, 2, 4,
+        '2023-05-04 22:28:50.215429');
 alter
     sequence ATTACHMENT_ID_SEQ restart with 1000;
+
 
 -- populate tasks
 delete
